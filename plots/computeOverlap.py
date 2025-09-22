00416022de8fda7cd3a470542c38a15026d631c5
procheta <senprocheta@gmail.com>
2025-09-22 20:44:20 +0000

    updated

--- plots/computeOverlap.py
+++ plots/computeOverlap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 df_edges = pd.read_csv('/home/ubuntu/MI_Bias/Dataset/topn_qwen_edges_updated-2.csv')
 
 top400_overlap_matrix = []
@@ -27,8 +27,9 @@ corr_matrix = np.array(corr_matrix)
 labels = ['Pos_DSS1', 'Neg_DSS1', 'Pos_DSS2','Male_GSS1', 'Female_GSS1', 'Male_GSS2', 'Female_GSS2', ]
 df = pd.DataFrame(corr_matrix, index=labels, columns=labels)
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 9))
 sns.heatmap(df, cmap='Blues', fmt=".2f")
-plt.title('Percentage Overlap: GPT2-Small (Top 400 edges)')
+plt.xticks(rotation=45)
+plt.title('Percentage Overlap: Qwen (Top 500 edges)')
 plt.savefig('Percentage Overlap M2C1 for GPT2-Small for all combinations(Top 400 edges).jpg')
 
